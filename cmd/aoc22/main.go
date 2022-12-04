7b51910472db680572c2d56a8117bd7f78d00e66
Isak Styf <isak.styf@masarinconsulting.se>
2022-12-04 15:04:33 +0100

    add ability to select challenge and input file via cli

--- cmd/aoc22/main.go
+++ cmd/aoc22/main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 
+	"github.com/istyf/aoc22/internal/pkg/aoc/calories"
 	"github.com/istyf/aoc22/internal/pkg/aoc/campcleanup"
+	"github.com/istyf/aoc22/internal/pkg/aoc/rps"
+	"github.com/istyf/aoc22/internal/pkg/aoc/rucksack"
 )
+
+var day string
+var inputFilePath string
+
+type AoCSolver func(io.Reader) (string, error)
 
 func main() {
 
-	inputfile, err := os.Open("./input.dat")
+	flag.StringVar(&day, "day", "4", "")
+	flag.StringVar(&inputFilePath, "input", "./input.dat", "The path to the inout file")
+	flag.Parse()
+
+	solutions := map[string][]AoCSolver{
+		"1": {calories.PartOne, calories.PartTwo},
+		"2": {rps.PartOne, rps.PartTwo},
+		"3": {rucksack.PartOne, rucksack.PartTwo},
+		"4": {campcleanup.PartOne, campcleanup.PartTwo},
+	}
+
+	inputfile, err := os.Open(inputFilePath)
 	if err != nil {
 		panic("unable to open input file")
 	}
@@ -23,15 +43,17 @@ func main() {
 		panic(err.Error())
 	}
 
-	result1, err := campcleanup.PartOne(bytes.NewBuffer(data))
-	if err != nil {
-		panic(err.Error())
+	solvers, ok := solutions[day]
+	if !ok {
+		panic("no solution found for that day")
 	}
 
-	result2, err := campcleanup.PartTwo(bytes.NewBuffer(data))
-	if err != nil {
-		panic(err.Error())
-	}
+	for part, solve := range solvers {
+		result, err := solve(bytes.NewBuffer(data))
+		if err != nil {
+			panic(err.Error())
+		}
 
-	fmt.Printf("Result; part one = %s, part two = %s", result1, result2)
+		fmt.Printf("Result for day %s part %d is %s\n", day, part+1, result)
+	}
 }
