d8e6a63f095738f57b9098c913d95ef2c1096206
Isak Styf <isak.styf@masarinconsulting.se>
2022-12-09 23:26:45 +0100

    refactor day 8

--- internal/pkg/aoc/treetoptreehouse/treetoptreehouse.go
+++ internal/pkg/aoc/treetoptreehouse/treetoptreehouse.go
@@ -8,62 +8,42 @@ import (
 )
 
 func PartOne(input io.Reader) (string, error) {
-	scanner := bufio.NewScanner(input)
-	scanner.Scan()
 
-	row := scanner.Text()
-	columnCount := len(row)
+	rows := loadForest(input)
+	columnCount := len(rows[0])
 
-	rows := append(make([][]tree, 0, columnCount), toarr(row))
-	rowIdx := 0
-
-	for idx := range rows[rowIdx] {
-		rows[rowIdx][idx].Visible = true
-	}
-
-	for scanner.Scan() {
-		rowIdx++
-		rows = append(rows, toarr(scanner.Text()))
-		rows[rowIdx][0].Visible = true
-		rows[rowIdx][columnCount-1].Visible = true
-	}
-
-	for idx := range rows[rowIdx] {
-		rows[rowIdx][idx].Visible = true
-	}
-
-	for _, trees := range rows {
-		highest := trees[0]
-		for i := 1; i < columnCount; i++ {
-			if !highest.obscures(trees[i]) {
-				trees[i].Visible = true
-				highest = trees[i]
-			}
-		}
-
-		highest = trees[columnCount-1]
-		for i := columnCount - 2; i >= 0; i-- {
-			if !highest.obscures(trees[i]) {
-				trees[i].Visible = true
-				highest = trees[i]
-			}
-		}
-	}
-
-	for x := columnCount - 2; x > 0; x-- {
-		highest := rows[0][x]
-		for y := 1; y < len(rows)-1; y++ {
-			if !highest.obscures(rows[y][x]) {
+	for y := len(rows) - 1; y >= 0; y-- {
+		highest := -1
+		for x := 0; x < columnCount-1; x++ {
+			if highest < rows[y][x].Height {
 				rows[y][x].Visible = true
-				highest = rows[y][x]
+				highest = rows[y][x].Height
 			}
 		}
 
-		highest = rows[len(rows)-1][x]
-		for y := len(rows) - 2; y > 0; y-- {
-			if !highest.obscures(rows[y][x]) {
+		highest = -1
+		for x := columnCount - 1; x > 0; x-- {
+			if highest < rows[y][x].Height {
 				rows[y][x].Visible = true
-				highest = rows[y][x]
+				highest = rows[y][x].Height
+			}
+		}
+	}
+
+	for x := columnCount - 2; x >= 0; x-- {
+		highest := -1
+		for y := 0; y < len(rows)-1; y++ {
+			if highest < rows[y][x].Height {
+				rows[y][x].Visible = true
+				highest = rows[y][x].Height
+			}
+		}
+
+		highest = -1
+		for y := len(rows) - 1; y > 0; y-- {
+			if highest < rows[y][x].Height {
+				rows[y][x].Visible = true
+				highest = rows[y][x].Height
 			}
 		}
 	}
@@ -82,19 +62,9 @@ func PartOne(input io.Reader) (string, error) {
 }
 
 func PartTwo(input io.Reader) (string, error) {
-	scanner := bufio.NewScanner(input)
-	scanner.Scan()
 
-	row := scanner.Text()
-	columnCount := len(row)
-
-	rows := append(make([][]tree, 0, columnCount), toarr(row))
-	rowIdx := 0
-
-	for scanner.Scan() {
-		rowIdx++
-		rows = append(rows, toarr(scanner.Text()))
-	}
+	rows := loadForest(input)
+	columnCount := len(rows[0])
 
 	boundsCheck := func(x, y int) (int, int, error) {
 		if x < 0 || y < 0 || x >= columnCount || y >= len(rows) {
@@ -106,12 +76,13 @@ func PartTwo(input io.Reader) (string, error) {
 
 	viewingDistanceFrom := func(x, y int, move func(int, int) (int, int)) int {
 		candidateTree := rows[y][x]
-		distance := 0
+		var err error
 
-		x, y, err := boundsCheck(move(x, y))
-		if err != nil {
-			return distance
+		if x, y, err = boundsCheck(move(x, y)); err != nil {
+			return 0
 		}
+
+		distance := 0
 
 		for err == nil {
 			distance++
@@ -150,7 +121,25 @@ func PartTwo(input io.Reader) (string, error) {
 	return strconv.FormatInt(int64(maxScenicScore), 10), nil
 }
 
-func toarr(row string) []tree {
+func loadForest(input io.Reader) [][]tree {
+	scanner := bufio.NewScanner(input)
+	scanner.Scan()
+
+	row := scanner.Text()
+	columnCount := len(row)
+
+	rows := append(make([][]tree, 0, columnCount), toTreeLine(row))
+	rowIdx := 0
+
+	for scanner.Scan() {
+		rowIdx++
+		rows = append(rows, toTreeLine(scanner.Text()))
+	}
+
+	return rows
+}
+
+func toTreeLine(row string) []tree {
 	arr := make([]tree, len(row))
 	for idx, tree := range row {
 		arr[idx] = *newTree(int(tree - '0'))
@@ -167,8 +156,4 @@ func newTree(height int) *tree {
 	return &tree{
 		Height: height,
 	}
-}
-
-func (t tree) obscures(other tree) bool {
-	return t.Height >= other.Height
 }
