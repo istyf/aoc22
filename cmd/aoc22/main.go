de40c186d91a5b66cd5d1f5269cfd966e31a1748
Isak Styf <isak.styf@masarinconsulting.se>
2022-12-11 12:38:32 +0100

    solution for day ten part one

--- cmd/aoc22/main.go
+++ cmd/aoc22/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/istyf/aoc22/internal/pkg/aoc/calories"
 	"github.com/istyf/aoc22/internal/pkg/aoc/campcleanup"
+	"github.com/istyf/aoc22/internal/pkg/aoc/crt"
 	"github.com/istyf/aoc22/internal/pkg/aoc/nospaceleft"
 	"github.com/istyf/aoc22/internal/pkg/aoc/ropebridge"
 	"github.com/istyf/aoc22/internal/pkg/aoc/rps"
@@ -25,20 +26,21 @@ type SolverFunc func(io.Reader) (string, error)
 
 func main() {
 
-	flag.StringVar(&day, "day", "9", "")
+	flag.StringVar(&day, "day", "10", "")
 	flag.StringVar(&inputFilePath, "input", "./input.dat", "The path to the inout file")
 	flag.Parse()
 
 	solutions := map[string][]SolverFunc{
-		"1": {calories.PartOne, calories.PartTwo},
-		"2": {rps.PartOne, rps.PartTwo},
-		"3": {rucksack.PartOne, rucksack.PartTwo},
-		"4": {campcleanup.PartOne, campcleanup.PartTwo},
-		"5": {supplystacks.PartOne, supplystacks.PartTwo},
-		"6": {tuningtrouble.PartOne, tuningtrouble.PartTwo},
-		"7": {nospaceleft.PartOne, nospaceleft.PartTwo},
-		"8": {treetoptreehouse.PartOne, treetoptreehouse.PartTwo},
-		"9": {ropebridge.PartOne, ropebridge.PartTwo},
+		"1":  {calories.PartOne, calories.PartTwo},
+		"2":  {rps.PartOne, rps.PartTwo},
+		"3":  {rucksack.PartOne, rucksack.PartTwo},
+		"4":  {campcleanup.PartOne, campcleanup.PartTwo},
+		"5":  {supplystacks.PartOne, supplystacks.PartTwo},
+		"6":  {tuningtrouble.PartOne, tuningtrouble.PartTwo},
+		"7":  {nospaceleft.PartOne, nospaceleft.PartTwo},
+		"8":  {treetoptreehouse.PartOne, treetoptreehouse.PartTwo},
+		"9":  {ropebridge.PartOne, ropebridge.PartTwo},
+		"10": {crt.PartOne},
 	}
 
 	inputfile, err := os.Open(inputFilePath)
--- internal/pkg/aoc/crt/crt.go
+++ internal/pkg/aoc/crt/crt.go
@@ -1,9 +1,44 @@
 package crt
 
 import (
+	"bufio"
 	"io"
+	"strconv"
+	"strings"
 )
 
 func PartOne(input io.Reader) (string, error) {
-	return "not implemented", nil
+	scanner := bufio.NewScanner(input)
+
+	accumulator := 0
+	clock := 0
+	register := 1
+
+	accumulate := func(clock, accumulator, register int) int {
+		if clock == 20 || (clock-20)%40 == 0 {
+			return accumulator + (clock * register)
+		}
+
+		return accumulator
+	}
+
+	for scanner.Scan() {
+		instruction := scanner.Text()
+		clock++
+
+		if instruction == "noop" {
+			accumulator = accumulate(clock, accumulator, register)
+		} else if strings.HasPrefix(instruction, "addx") {
+			accumulator = accumulate(clock, accumulator, register)
+			accumulator = accumulate(clock+1, accumulator, register)
+			clock++
+
+			amount, _ := strconv.ParseInt(strings.Split(instruction, " ")[1], 10, 64)
+			register += int(amount)
+		} else {
+			panic("cpu halted; illegal instruction")
+		}
+	}
+
+	return strconv.FormatInt(int64(accumulator), 10), nil
 }
